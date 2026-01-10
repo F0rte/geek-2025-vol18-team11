52f8dbf45d0319633e582244ca1070e6f2cfb60d
f0rte <y4859.rikoo_phy-sur21@outlook.jp>
2026-01-10 20:14:41 +0900

    fix: resolve problems

--- backend/main.py
+++ backend/main.py
@@ -4,14 +4,14 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from mangum import Mangum
 
-FRONTEND_URL = os.environ.get("FRONTEND_URL")
-if not FRONTEND_URL:
+frontend_url = os.environ.get("FRONTEND_URL")
+if not frontend_url:
     raise RuntimeError("FRONTEND_URL must be set")
 
 app = FastAPI()
 
 # CORS
-origins = [FRONTEND_URL]
+origins = [frontend_url]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
